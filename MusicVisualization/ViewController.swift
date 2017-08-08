855da67173f0a2d2241fec65feba1c00487f2f23
黃柏叡 <rick.huang@phoenixnet.com.tw>
2017-08-08 16:19:10 +0800

    Initial Commit

---
+++ MusicVisualization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVisualization
+//
+//  Created by 黃柏叡 on 2017/8/8.
+//  Copyright © 2017年 黃柏叡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
